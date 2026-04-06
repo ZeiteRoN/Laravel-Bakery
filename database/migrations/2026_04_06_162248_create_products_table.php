979141d2c95520e28d5091da96aad926a71c3378
stars <zeiter1007@gmail.com>
2026-04-06 19:27:36 +0300

    added Migrations(User, Category, Product, Order, Order_item)

---
+++ database/migrations/2026_04_06_162248_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->float('price')->default(0.00);
+            $table->float("weight")->default(0.00);
+            $table->float("height")->default(0.00);
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->string('image_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
